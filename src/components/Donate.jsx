c145a0ba08c14b93ba4e86c354789b8c2a7c3990
Ahmad7264 <dilshadahmad286@gmail.com>
2024-04-25 23:27:56 +0530

    changes in Donate page

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,6 +1,13 @@
 import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import image1 from "../assets/img1.png";
+import image2 from "../assets/img2.png";
+import image3 from "../assets/img4.png";
+import image4 from "../assets/mainimg.png";
+import image5 from "../assets/mainimg2.png";
+import image6 from "../assets/education.png";
+
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -88,20 +95,19 @@ const Donate = () => {
         initial="hidden"
         whileInView={"show"}
         viewport={{ once: false, amount: 0.6 }}
-        className="min-h-screen flex flex-col justify-center  sm:px-14 lg:px-32 px-5 text-white bg-[url('assets/main4.png')] bg-no-repeat bg-cover opacity-90"
+        className="min-h-screen flex flex-col justify-center  sm:px-14 lg:px-32 px-5 text-white bg-[url('assets/main3.png')] bg-no-repeat bg-cover opacity-90"
       >
         <motion.div
-          animate={{ x: 0 }} initial={false}
           // variants={fadeIn("left", 0.2)}
           // initial="hidden"
           // whileInView={"show"}
           // viewport={{ once: false, amount: 0.6 }}
           className=" w-full lg:w-4/5 space-y-5 "
         >
-          <h1 className="text-6xl font-bold mb-4   text-bPrimary md:w-3/4  leading-snug">
+          <h1 className="text-6xl font-bold mb-4 text-bPrimary font-serif md:w-3/4  leading-snug">
             Renu Health <span className="text-dGrey">Care</span>
           </h1>
-          <p className="text-bold font-bold hover:text-bPrimary   ">
+          <p className="text-bold font-bold hover:text-bPrimary  font-serif ">
             If your project grows in size, this option is available.<br></br>
             This method has the trade-off that it takes<br></br>a long time to
             install the package.<br></br>
@@ -114,32 +120,30 @@ const Donate = () => {
         </motion.div>
       </motion.div>
       {/* 2nd Section start */}
-      <div
-        // variants={fadeIn("up", 0.2)}
-        // initial="hidden"
-        // whileInView={"show"}
-        // viewport={{ once: false, amount: 0.6 }}
+      <motion.div
+        variants={fadeIn("up", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.6 }}
         className="min-h-screen flex flex-col justify-center lg:px-32 pt-2
         "
       >
         <div
-          // variants={fadeIn("up", 0.2)}
-          // initial="hidden"
-          // whileInView={"show"}
-          // viewport={{ once: false, amount: 0.6 }}
-          className="flex flex-col items-center lg:flex-row justify-center mb-10 lg:mb-0"
+        // variants={fadeIn("up", 0.2)}
+        // initial="hidden"
+        // whileInView={"show"}
+        // viewport={{ once: false, amount: 0.6 }}
+        // className="flex flex-col items-center lg:flex-row justify-center mb-10 lg:mb-0"
         >
           <div>
-            <h1 className="text-4xl font-semibold text-center justify-center mt-0  lg:text-start">
-              Rise Your Hand To Help The Poor Children
+            <h1 className="text-4xl font-semibold text-center justify-center mt-0  lg:text-start font-serif">
+              Rise Your Hand To Help The Poor Family
             </h1>
-            <p className="mt-2 text-center justify-center  lg:text-start">
-              he trade-off that it takes a long time to install the package.
-            </p>
+            
           </div>
-          <div>
+          <div className="flex float-end">
             <button
-              className=" text-bPrimary px-4 mt-20 py-2 rounded-lg  active:bg-slate-600"
+              className=" text-bPrimary px-4  py-2 rounded-lg  active:bg-slate-600"
               onClick={() => slider.current.slickPrev()}
             >
               <FaArrowLeft size={25} />
@@ -152,7 +156,7 @@ const Donate = () => {
             </button>
           </div>
         </div>
-        <div className="mt-5">
+        <div className="mt-5 mb-0">
           <Slider ref={slider} {...settings}>
             {data.map((e, index) => (
               <div
@@ -173,7 +177,107 @@ const Donate = () => {
             ))}
           </Slider>
         </div>
+      </motion.div>
+
+      {/* card items */}
+      <div className=" max-w-full mx-auto my-5">
+        <h1 className=" text-4xl text-center font-bold text-dGrey hover:text-bPrimary tracking-widest font-serif">
+          Donate to the Ngo
+        </h1>
       </div>
+      <div className=" max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-5 px-8">
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image1}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Poor children
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image2}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Better LIfe
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image3}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Home
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image4}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Food
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image5}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Better Food
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+        <div className="text-center shadow-xl mb-11 rounded-xl ">
+          <div className=" overflow-hidden">
+            <img
+              src={image6}
+              className=" h-96 object-cover hover:scale-110 duration-1000 rounded-t-xl  "
+              alt=""
+            />
+          </div>
+          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
+            Give for Education
+          </h2>
+          <p className="py-2">
+            Rapidly build modern websites without ever leaving your HTML.
+          </p>
+        </div>
+      </div>
+
       {/* donation form */}
     </>
   );
